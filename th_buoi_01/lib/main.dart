f9a59bc4c48181cc57513900daaa5d2574db75fc
bladeonfire2402 <liemp966@gmail.com>
2025-06-17 17:46:35 +0700

    buoi 1

---
+++ th_buoi_01/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:th_buoi_01/screen/login-screen/index.dart';
+import 'package:th_buoi_01/screen/welcome-screen/index.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
