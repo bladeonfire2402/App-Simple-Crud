564ce01e8c5e23302a08750941f97e2ffaa12c0e
bladeonfire2402 <liemp966@gmail.com>
2025-08-01 23:48:03 +0700

    remove unessesary

--- th_meoemeo/android/app/src/main/kotlin/com/example/th_meoemeo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.th_meoemeo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- th_speedrun/android/app/src/main/kotlin/com/example/th_speedrun/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.th_speedrun
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
